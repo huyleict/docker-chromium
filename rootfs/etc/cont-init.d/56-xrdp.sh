2702408382318ffb49261d86ad2b773853560a32
huyleict <huyle.ict@gmail.com>
2026-09-16 15:19:25 +0700

    fix: make XRDP credentials readable by service user

--- rootfs/etc/cont-init.d/56-xrdp.sh
+++ rootfs/etc/cont-init.d/56-xrdp.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+
 set -eu
+
 XRDP_USERNAME=${XRDP_USERNAME:-abc}
 XRDP_PASSWORD=${XRDP_PASSWORD:-${VNC_PASSWORD:-}}
 [ -n "$XRDP_PASSWORD" ] || exit 0
-if ! id "$XRDP_USERNAME" >/dev/null 2>&1; then adduser -D -s /bin/sh "$XRDP_USERNAME"; fi
+
+if ! id "$XRDP_USERNAME" >/dev/null 2>&1; then
+    adduser -D -s /bin/sh "$XRDP_USERNAME"
+fi
+
 printf '%s:%s\n' "$XRDP_USERNAME" "$XRDP_PASSWORD" | chpasswd
+
+for XRDP_FILE in /etc/xrdp/rsakeys.ini /etc/xrdp/cert.pem /etc/xrdp/key.pem
+do
+    [ -f "$XRDP_FILE" ] && chmod 644 "$XRDP_FILE"
+done
