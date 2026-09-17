28217ed109256a489c3869e2620024912c82267b
huyleict <huyle.ict@gmail.com>
2026-09-17 15:08:29 +0700

    fix: initialize UniKey after Chromium starts

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -26,6 +26,12 @@ fi
 
 fcitx5 -D --replace >> "$FCITX_LOG" 2>&1 &
 
+/usr/bin/chromium-browser "$@" >> "$CHROMIUM_OUTPUT_LOG" 2>> "$CHROMIUM_ERROR_LOG" &
+CHROMIUM_PID=$!
+
+sleep 1
+kill -0 "$CHROMIUM_PID" 2>/dev/null || wait "$CHROMIUM_PID"
+
 for _ in 1 2 3 4 5 6 7 8 9 10
 do
     FCITX_ENV=$(tr "\0" "\n" < /proc/$(pgrep -o fcitx5)/environ 2>/dev/null |
@@ -35,6 +41,6 @@ do
     sleep 1
 done
 
-exec /usr/bin/chromium-browser "$@" >> "$CHROMIUM_OUTPUT_LOG" 2>> "$CHROMIUM_ERROR_LOG"
+wait "$CHROMIUM_PID"
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
