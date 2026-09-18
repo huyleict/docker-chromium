e17cdb5545ad6a8ae57dfee7b2e32c77dacc7825
huyleict <huyle.ict@gmail.com>
2026-09-18 10:51:49 +0700

    fix xorg display environment

--- rootfs-xorg/etc/xrdp/startwm.sh
+++ rootfs-xorg/etc/xrdp/startwm.sh
@@ -2,6 +2,8 @@
 set -eu
 
 export HOME=/config
+export DISPLAY="${DISPLAY:-:${XRDP_DISPLAY:-10}}"
+export XAUTHORITY="${XAUTHORITY:-/config/.Xauthority}"
 export XDG_CONFIG_HOME=/config/.config
 export LANG=${LANG:-C.UTF-8}
 export GTK_IM_MODULE=fcitx
